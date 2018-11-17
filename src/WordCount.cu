85ca0fe2779a99925815b9a7e385b317ce0d5756
SiyuZheng <siyuz@seas.upenn.edu>
2018-11-17 12:18:52 -0500

    readline

--- src/WordCount.cu
+++ src/WordCount.cu
@@ -10,6 +10,9 @@
 #include <thrust/device_vector.h>
 #include <thrust/sort.h>
 #include "device_launch_parameters.h"
+#include <sstream>
+#include <string>
+#include <fstream>
 
 #define FILENAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #define checkCUDAError(msg) checkCUDAErrorFn(msg, FILENAME, __LINE__)
@@ -40,6 +43,7 @@ __device__ void mapper(char *input, KeyValuePair *pairs)
 	{
 		pairs->value = 0;
 	}
+	
 }
 
 
@@ -61,12 +65,6 @@ __global__ void kernReduce(KeyValuePair *pairs, int *odata) {
 		for (j = 1; j < NUM_INPUT * NUM_KEYS; j++) {
 			if (KVComparator()(pairs[j - 1], pairs[j])) {
 				if (count == i) {
-					// This thread has found the correct number
-					// There is a bit of warp divergence here as some threads
-					// break before others, but we still make the most out of it
-					// by calling the reducer at the very end, so there is not
-					// any warp divergence where the bulk of the computation
-					// should occur (the reducer).
 					break;
 				}
 				else {
@@ -115,40 +113,22 @@ void cudaMapReduce(char* input, int *output) {
 	cudaFree(dev_odata);
 }
 
-const int N = 20;
-const int blocksize = 20;
-
-__global__
-void hello(char *a, int *c, int size)
-{
-	int i = 0, k = 0;
-	int count = 1;
-
-	if (threadIdx.x < size) {
-
-		for (i = 0; i < size; i++) {
-			if (threadIdx.x != i) {
-				for (k = 0; k < N; k++) {
-					if (a[N * threadIdx.x + k] != a[N * i + k]) {
-						break;
-					}
-					if (k == N - 1) {
-						count++;
-					}
-				}
-			}
-		}
-
-		c[threadIdx.x] = count;
-	}
-	else {
-		c[threadIdx.x] = -1;
-	}
-}
-
 int main(int argc, char* argv[]) {
+	//// read by line
+	//std::ifstream infile("hamlet.txt");
+	//std::string line;
+	//if (infile) {
+	//	while (std::getline(infile, line)) {
+	//		std::cout << line << std::endl;
+	//	}
+	//}
+	//else {
+	//	std::cout << "no such file" << std::endl;
+	//}
+
 	char* idata = new char[NUM_INPUT];
 	int* odata = new int[NUM_OUTPUT];
+	
 	FILE* fp;
 	fp = fopen("hamlet.txt", "r");
 	int i = 0;
@@ -172,86 +152,9 @@ int main(int argc, char* argv[]) {
 	cudaMapReduce(idata, odata);
 	for (int i = 0; i < NUM_OUTPUT; i++)
 	{
-		printf("The total number of words in the file are: %d\n", odata[i]);
+		printf("Total word count %d\n", odata[i]);
 	}
 	delete idata;
 	delete odata;
 	return 0;
-	//char words[N][N] =
-	//{
-	//	"ABCDE",
-	//	"xyz",
-	//	"Hi",
-	//	"japan",
-	//	"xyz",
-	//	"Hi",
-	//	"cup",
-	//	"paper",
-	//	"Hi",
-	//	"Apple"
-	//};
-
-	//int size = 10;
-
-	//int count[N];
-
-	//char answer_words[N][N];
-	//int answer_count[N];
-
-	//char *ad;
-	//int *cd;
-
-	//const int csize = N*N * sizeof(char);
-	//const int isize = N * sizeof(int);
-
-	//cudaMalloc((void**)&ad, csize);
-	//cudaMalloc((void**)&cd, isize);
-
-	//cudaMemcpy(ad, words, csize, cudaMemcpyHostToDevice);
-
-	//dim3 dimBlock(blocksize, 1);
-	//dim3 dimGrid(1, 1);
-	//hello << <dimGrid, dimBlock >> >(ad, cd, size);
-	//cudaMemcpy(count, cd, isize, cudaMemcpyDeviceToHost);
-	//cudaFree(ad);
-	//cudaFree(cd);
-
-	//int i = 0, k = 0;
-
-	//int num = 0;
-	//int dismatchflag = 0;
-
-	//for (i = 0; i < N; i++) {
-	//	if (count[i] == -1) {
-	//		break;
-	//	}
-
-	//	if (count[i] == 1) {
-	//		strcpy(answer_words[num], words[i]);
-	//		answer_count[num] = count[i];
-	//		num++;
-	//	}
-	//	else if (count[i] > 1) {
-	//		for (k = 0; k < num; k++) {
-	//			if (strcmp(words[i], answer_words[k]) == 0) {
-	//				dismatchflag = 1;
-	//				break;
-	//			}
-	//		}
-	//		if (dismatchflag == 0) {
-	//			strcpy(answer_words[num], words[i]);
-	//			answer_count[num] = count[i];
-	//			num++;
-	//		}
-	//		else {
-	//			dismatchflag = 0;
-	//		}
-	//	}
-	//}
-
-	//for (i = 0; i < num; i++) {
-	//	printf("%s, %d\n", answer_words[i], answer_count[i]);
-	//}
-
-	//return EXIT_SUCCESS;
 }
